3a8f9c371cdc6372bde27910b509437300ee9d71
Viviana Herrera <vh89445@usc.edu>
2025-04-23 07:57:57 +0000

    Fully Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,54 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool completeSchedule(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched,
+                    vector<int>& shiftTracker, size_t day, size_t slot)
+{
+    if(day == avail.size())
+    {
+        return true;
+    }
+    for(size_t w = 0; w < avail[0].size(); w++)
+    {
+        if(!avail[day][w])
+        {
+            continue;
+        }
+        if(shiftTracker[w] >= (int)maxShifts)
+        {
+            continue;
+        }
+        bool alreadyWorking = false;
+        for(size_t i = 0; i < sched[day].size(); i++)
+        {
+            if(sched[day][i] == w)
+            {
+                alreadyWorking = true;
+                break;
+            }
+        }
+        if(alreadyWorking)
+        {
+            continue;
+        }
+        sched[day][slot] = w;
+        shiftTracker[w]++;
+        size_t next = day;
+        size_t nextSlot = slot + 1;
+        if(nextSlot == dailyNeed)
+        {
+            next++;
+            nextSlot = 0;
+        }
+        if(completeSchedule(avail, dailyNeed, maxShifts, sched, shiftTracker, next, nextSlot))
+        {
+            return true;
+        }
+        sched[day][slot] = INVALID_ID;
+        shiftTracker[w]--;
+    }
+    return false;
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +84,16 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t days = avail.size();
+    size_t workers = avail[0].size();
 
+    if(dailyNeed > workers)
+    {
+        return false;
+    }
 
-
-
+    sched.assign(days, std::vector<Worker_T>(dailyNeed, INVALID_ID));
+    std::vector<int> shiftTracker(workers, 0);
+    
+    return completeSchedule(avail, dailyNeed, maxShifts, sched, shiftTracker, 0, 0);
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,72 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void recurseFun(std::string& curr, const std::string& floating, std::set<std::string>& results, const std::set<std::string>& dict, size_t idx)
+{
+	if(idx == curr.length())
+	{
+		std::string temp = floating;
+		for(size_t i = 0; i < curr.length(); i++)
+		{
+			size_t pos = temp.find(curr[i]);
+			if(pos != std::string::npos)
+			{
+				temp.erase(pos, 1);
+			}
+		}
+		if(temp.empty() && dict.find(curr) != dict.end())
+		{
+			results.insert(curr);
+		}
+		return;
+	}
 
+	if(curr[idx] != '-')
+	{
+		recurseFun(curr, floating, results, dict, idx + 1);
+		return;
+	}
+	size_t blanks = 0;
+	for(size_t i = idx; i < curr.length(); i++)
+	{
+		if(curr[i] == '-')
+		{
+			blanks++;
+		}
+	}
+
+	if(blanks <= floating.size())
+	{
+		for(size_t i = 0; i < floating.size(); i++)
+		{
+			char c = floating[i];
+			curr[idx] = c;
+			std::string newFloating = floating;
+			newFloating.erase(i, 1);
+			recurseFun(curr, newFloating, results, dict, idx + 1);
+			curr[idx] = '-';
+		}
+	}
+	else
+	{
+		for(char c = 'a'; c <= 'z'; c++)
+		{
+			curr[idx] = c;
+			size_t pos = floating.find(c);
+			if(pos != string::npos)
+			{	
+				string newFloating = floating;
+				newFloating.erase(pos, 1);
+				recurseFun(curr, newFloating, results, dict, idx + 1);
+			}
+			else
+			{
+				recurseFun(curr, floating, results, dict, idx + 1);
+			}
+			curr[idx] = '-';
+		}
+	}
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,6 +87,14 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+		if(in.empty())
+		{
+			return {};
+		}
+		std::set<std::string> results;
+		std::string curr = in;
+		recurseFun(curr, floating, results, dict, 0);
+		return results;
 
 }
 
